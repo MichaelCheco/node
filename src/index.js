645afd36e3755a498002a5d7782171724af3077b
Michael <checomichael2@gmail.com>
2019-03-18 15:22:22 -0400

    added user resource

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import express from 'express';
 import cors from 'cors';
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => {
-	return res.send('Received a GET HTTP method');
+app.get('/users', (req, res) => {
+	return res.send('GET HTTP method on user resource');
 });
 
-app.post('/', (req, res) => {
-	return res.send('Received a POST HTTP method');
+app.post('/users', (req, res) => {
+	return res.send('POST HTTP method on user resource');
 });
 
-app.put('/', (req, res) => {
-	return res.send('Received a PUT HTTP method');
+app.put('/users', (req, res) => {
+	return res.send('PUT HTTP method on user resource');
 });
 
-app.delete('/', (req, res) => {
-	return res.send('Received a DELETE HTTP method');
+app.delete('/users', (req, res) => {
+	return res.send('DELETE HTTP method on user resource');
 });
 
 app.listen(3000, () =>
