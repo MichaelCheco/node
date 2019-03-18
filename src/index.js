179809c60905ebc0fdab58d51ea77ba4c85f433a
Michael <checomichael2@gmail.com>
2019-03-18 15:48:22 -0400

    added dummy data and routes to use data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,55 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+let users = {
+	1: {
+		id: '1',
+		username: 'Robin Wieruch',
+	},
+	2: {
+		id: '2',
+		username: 'Dave Davids',
+	},
+};
+
+let messages = {
+	1: {
+		id: '1',
+		text: 'Hello World',
+		userId: '1',
+	},
+	2: {
+		id: '2',
+		text: 'By World',
+		userId: '2',
+	},
+};
 const app = express();
 app.use(cors());
 app.get('/users', (req, res) => {
-	return res.send('GET HTTP method on user resource');
+	return res.send(Object.values(users));
+});
+app.get('/users/:userId', (req, res) => {
+	const { userId } = req.params;
+	return res.send(users[userId]);
+});
+app.get('/messages', (req, res) => {
+	return res.send(Object.values(messages));
 });
 
+app.get('/messages/:messageId', (req, res) => {
+	return res.send(messages[req.params.messageId]);
+});
 app.post('/users', (req, res) => {
 	return res.send('POST HTTP method on user resource');
 });
 
-app.put('/users', (req, res) => {
-	return res.send('PUT HTTP method on user resource');
+app.put('/users/:userId', (req, res) => {
+	return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
 });
 
-app.delete('/users', (req, res) => {
-	return res.send('DELETE HTTP method on user resource');
+app.delete('/users/:userId', (req, res) => {
+	return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
 app.listen(3000, () =>
